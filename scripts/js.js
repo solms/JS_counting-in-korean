8f1eff89c418fc5b2a9d6d3e8d4b7cdb2902c6b5
Solms Dykman <solmsd@gmail.com>
2015-04-30 14:51:24 +0900

    Fix duplicate answers appearing, add continue button, add some comments

--- scripts/js.js
+++ scripts/js.js
@@ -1,34 +1,53 @@
+// Array to store answers in order to check for duplicates
+var used = [];
 
-$("#answer1").click(function(){
-    alert('I got a click');
-});
+// Generates the question
 function generate_question() {
+	// Hide the 'next question' button
+	document.getElementById("continue").innerHTML = ""
+	// Clear the answers array when a new question is generated
+	used = [];
+	// Pick a random number [0,10) to ask
 	var num = Math.floor(Math.random()*10);
-	document.getElementById("question").innerHTML = "<strong>What is " + num + " in Korean?</strong>"
+	// Add it to the used array
+	used[used.length] = num;
+	// Display the question using the numeral
+	document.getElementById("question").innerHTML = "<p>What is " + num + " in Korean?</p>"
+	// Place the correct answer at a random position
 	var x = Math.floor(Math.random()*4+1)
 	document.getElementById("answer" + x).innerHTML = "<p id='correct' onclick='show_result(1)'>" + converter(num) + "</p>";
+	// Place randomly generated incorrect answers at random positions
 	for (i = 1; i < 5; i++) {
 		if (i!=x) {
-			var y = Math.floor(Math.random()*10);
+			do {
+				var y = Math.floor(Math.random()*10);
+			} while (used.indexOf(y) > -1) // Make sure that answers don't duplicate
+			used[used.length] = y;
 			document.getElementById("answer" + i).innerHTML = "<p id='incorrect' onclick='show_result(0)'>" + converter(y) + "</p>";
 		}
 	}
 }
 
+// Show the result of the answer after being clicked
 function show_result(ans){
 	if(ans) {
 		document.getElementById("result").innerHTML = "CORRECT! :)";
+		document.getElementById("continue").innerHTML = "<button type='button' onclick='generate_question()'>Next question</button>"
 	} else {
 		document.getElementById("result").innerHTML = "INCORRECT! :(";
 	}
 }
 
 function converter(num) {
-	if (num > 10) {
+	var korean_num = "";
+	if (num > 100) {
+
+	} else if (num > 10) {
 
 	} else {
-		return get_korean(num);
+		korean_num += get_korean(num);
 	}
+	return korean_num;
 }
 
 function get_korean(num) {
